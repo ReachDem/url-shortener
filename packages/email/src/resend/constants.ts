f3858527b51a42448b081e8d66e84c9a2a4fc55e
Belrick Stephane <73307766+latioms@users.noreply.github.com>
2025-08-05 15:22:22 +0100

    Update email constants for audience and sender clarity

--- packages/email/src/resend/constants.ts
+++ packages/email/src/resend/constants.ts
@@ -1,10 +1,10 @@
 export const RESEND_AUDIENCES = {
-  "app.dub.co": "f5ff0661-4234-43f6-b0ca-a3f3682934e3",
+  "Reachdem users": "9542812b-51eb-48e3-87b2-e9fb152e3fb5",
   "partners.dub.co": "6caf6898-941a-45b6-a59f-d0780c3004ac",
 };
 
 export const VARIANT_TO_FROM_MAP = {
-  primary: "Dub.co <system@dub.co>",
-  notifications: "Dub.co <notifications@mail.dub.co>",
-  marketing: "Steven from Dub.co <steven@ship.dub.co>",
+  primary: "Dub.co <mail@updates.reachdem.cc>",
+  notifications: "Dub.co <mail@updates.reachdem.cc>",
+  marketing: "Belrick from Reachdem <berlrick@updates.reachdem.cc>",
 };
